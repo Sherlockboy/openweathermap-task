b07383582141e189da9c36df1541e08a8f2a3b0c
Aziz Riskulov <vinceere@gmail.com>
2024-08-30 18:32:01 +0500

    Add migrations for cities and weather tables, update .gitignore

---
+++ database/migrations/2024_08_29_174214_create_weather_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('weather', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')->constrained()->onDelete('cascade');
+            $table->dateTime('time');
+            $table->string('weather_name');
+            $table->decimal('temperature', 5, 2);
+            $table->decimal('min_temperature', 5, 2);
+            $table->decimal('max_temperature', 5, 2);
+            $table->integer('pressure');
+            $table->integer('humidity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('weather');
+    }
+};
